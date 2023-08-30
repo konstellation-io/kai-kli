0d1e9eddc223920d8b6a4e7ddf9f849c48499b9f
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2023-08-30 10:13:08 +0200

    add renamed giles

---
+++ cmd/processregistry/process_registry.go
@@ -0,0 +1,28 @@
+package processregistry
+
+import (
+	"github.com/MakeNowJust/heredoc"
+	"github.com/spf13/cobra"
+
+	"github.com/konstellation-io/kli/internal/logging"
+)
+
+// NewProcessRegistryCmd creates a new command to handle 'process-registry' subcommands.
+func NewProcessRegistryCmd(logger logging.Interface) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "process-registry <command>",
+		Short: "Manage process-registry",
+		Example: heredoc.Doc(`
+			$ kli process-registry ls <product_id> [--type <process_type>] [opts...]
+			$ kli process-registry register <process_type> <process_id> [opts...]
+			$ kli process-registry unregister <process_id> [opts...]
+		`),
+	}
+
+	cmd.AddCommand(
+		NewListCmd(logger),
+		NewRegisterCmd(logger),
+	)
+
+	return cmd
+}
---
+++ internal/commands/processregistry/process_registry.go
@@ -0,0 +1,29 @@
+package processregistry
+
+import (
+	"github.com/konstellation-io/kli/api/processregistry"
+	"github.com/konstellation-io/kli/authserver"
+	"github.com/konstellation-io/kli/internal/logging"
+	"github.com/konstellation-io/kli/internal/render"
+	"github.com/konstellation-io/kli/internal/services/auth"
+	"github.com/konstellation-io/kli/internal/services/configuration"
+)
+
+type Handler struct {
+	logger                logging.Interface
+	renderer              render.Renderer
+	processRegistryClient processregistry.ProcessRegistryInterface
+	authentication        *auth.AuthenticationService
+	configService         *configuration.KaiConfigService
+}
+
+func NewHandler(logger logging.Interface, renderer render.Renderer,
+	processRegistryCli processregistry.ProcessRegistryInterface) *Handler {
+	return &Handler{
+		logger:                logger,
+		renderer:              renderer,
+		processRegistryClient: processRegistryCli,
+		authentication:        auth.NewAuthentication(logger, authserver.NewDefaultAuthServer(logger)),
+		configService:         configuration.NewKaiConfigService(logger),
+	}
+}
