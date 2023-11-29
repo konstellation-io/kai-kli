ff130dcb3a869d1b7180338c87fef5fcdb52fc82
Alex Rodriguez Fernandez <alexrodfe@gmail.com>
2023-11-29 11:29:29 +0100

    feature(kai-kli): Add warning when starting critical status versions (#62)

--- internal/commands/version/start_test.go
+++ internal/commands/version/start_test.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 
 	"github.com/konstellation-io/kli/internal/commands/version"
+	"github.com/konstellation-io/kli/internal/entity"
 )
 
 func (s *VersionSuite) TestStartVersion() {
+	s.versionClient.EXPECT().Get(s.server, productName, versionTag).Return(&entity.Version{}, nil).Once()
 	s.versionClient.EXPECT().Start(s.server, productName, versionTag, comment).Return(versionTag, nil).Once()
 
 	err := s.handler.Start(&version.StartOpts{
@@ -21,6 +23,7 @@ func (s *VersionSuite) TestStartVersion() {
 func (s *VersionSuite) TestStartVersion_ErrorIfClientFails() {
 	expectedError := errors.New("client error")
 
+	s.versionClient.EXPECT().Get(s.server, productName, versionTag).Return(&entity.Version{}, nil).Once()
 	s.versionClient.EXPECT().Start(s.server, productName, versionTag, comment).Return("", expectedError).Once()
 
 	err := s.handler.Start(&version.StartOpts{
