528baf01139650f97b79392378cf885248757fce
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2024-02-22 14:16:06 +0100

    fix: product ls with filters

--- internal/commands/product/list.go
+++ internal/commands/product/list.go
@@ -1,8 +1,15 @@
 package product
 
-import "github.com/konstellation-io/kli/internal/services/configuration"
+import (
+	"github.com/konstellation-io/kli/api/kai"
+	"github.com/konstellation-io/kli/internal/services/configuration"
+)
 
-func (h *Handler) ListProducts(serverName string) error {
+type ListProductsOpts struct {
+	ProductName string
+}
+
+func (h *Handler) ListProducts(serverName string, opts *ListProductsOpts) error {
 	configService := configuration.NewKaiConfigService(h.logger)
 
 	conf, err := configService.GetConfiguration()
@@ -20,7 +27,26 @@ func (h *Handler) ListProducts(serverName string) error {
 		return err
 	}
 
+	// TODO - technical debt, this should be done in the server side
+	// filter is done here instead of in the client to avoid adding complexity to the client
+	// and make the rendering of empty results after filtering more easy to manage
+	if opts.ProductName != "" {
+		products = filterProductsByName(products, opts.ProductName)
+	}
+
 	h.renderer.RenderProducts(products)
 
 	return nil
+}
+
+func filterProductsByName(products []kai.Product, name string) []kai.Product {
+	var filteredProducts []kai.Product
+
+	for _, p := range products {
+		if p.Name == name {
+			filteredProducts = append(filteredProducts, p)
+		}
+	}
+
+	return filteredProducts
 }
