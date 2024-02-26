24bae5f2b5ca5a92e89b220943489d8eea91a91d
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2024-02-26 20:10:34 +0100

    feat: use new status filter for product version list endpoint

--- internal/commands/version/list.go
+++ internal/commands/version/list.go
@@ -1,11 +1,9 @@
 package version
 
-import "github.com/konstellation-io/kli/internal/entity"
-
 type ListVersionsOpts struct {
 	ServerName   string
 	ProductID    string
-	StatusFilter string
+	StatusFilter *string
 }
 
 func (h *Handler) ListVersions(opts *ListVersionsOpts) error {
@@ -19,31 +17,12 @@ func (h *Handler) ListVersions(opts *ListVersionsOpts) error {
 		return err
 	}
 
-	registeredProcesses, err := h.versionClient.List(srv, opts.ProductID)
+	registeredProcesses, err := h.versionClient.List(srv, opts.ProductID, opts.StatusFilter)
 	if err != nil {
 		return err
-	}
-
-	// TODO - technical debt, this should be done in the server side
-	// filter is done here instead of in the client to avoid adding complexity to the client
-	// and make the rendering of empty results after filtering more easy to manage
-	if opts.StatusFilter != "" {
-		registeredProcesses = filterVersionsByStatus(registeredProcesses, opts.StatusFilter)
 	}
 
 	h.renderer.RenderVersions(opts.ProductID, registeredProcesses)
 
 	return nil
-}
-
-func filterVersionsByStatus(versions []*entity.Version, status string) []*entity.Version {
-	var filteredVersions []*entity.Version
-
-	for _, v := range versions {
-		if v.Status == status {
-			filteredVersions = append(filteredVersions, v)
-		}
-	}
-
-	return filteredVersions
 }
