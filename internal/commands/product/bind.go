ade8f01529e7ce7d941455b49b5d403d1a961b81
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2024-02-28 14:23:22 +0100

    fix: bind when product exists but has no version

--- internal/commands/product/bind.go
+++ internal/commands/product/bind.go
@@ -2,11 +2,16 @@ package product
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/konstellation-io/kli/internal/entity"
 	"github.com/konstellation-io/kli/internal/services/configuration"
 	productconfiguration "github.com/konstellation-io/kli/internal/services/product_configuration"
 	"github.com/konstellation-io/kli/pkg/krtutil"
+)
+
+var (
+	ErrProductExistsNoVersions = errors.New("error product exists but has no versions")
 )
 
 type BindProductOpts struct {
@@ -30,13 +35,6 @@ func (h *Handler) Bind(opts *BindProductOpts) error {
 		return err
 	}
 
-	var version *entity.Version
-
-	version, err = h.versionClient.Get(server, opts.ProductID, opts.VersionTag)
-	if err != nil {
-		return err
-	}
-
 	productConfig, err := h.configService.GetConfiguration(opts.ProductID, opts.LocalPath)
 	if err != nil && !errors.Is(err, productconfiguration.ErrProductConfigNotFound) {
 		return err
@@ -44,6 +42,11 @@ func (h *Handler) Bind(opts *BindProductOpts) error {
 
 	if productConfig != nil && !opts.Force {
 		return ErrProductConfigurationAlreadyExists
+	}
+
+	version, err := h.getVersion(server, opts)
+	if err != nil {
+		return err
 	}
 
 	err = h.configService.WriteConfiguration(&productconfiguration.KaiProductConfiguration{
@@ -57,4 +60,18 @@ func (h *Handler) Bind(opts *BindProductOpts) error {
 	h.renderer.RenderProductBinded(opts.ProductID)
 
 	return nil
+}
+
+func (h *Handler) getVersion(server *configuration.Server, opts *BindProductOpts) (*entity.Version, error) {
+	version, err := h.versionClient.Get(server, opts.ProductID, opts.VersionTag)
+	if err != nil {
+		if strings.Contains(err.Error(), ErrProductExistsNoVersions.Error()) {
+			h.logger.Warn("Product exists but has no versions, using an empty version")
+			version = &entity.Version{}
+		} else {
+			return nil, err
+		}
+	}
+
+	return version, nil
 }
